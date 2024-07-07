c9d70421eec92c0b4a7faa61e35761c4a072aaaf
Chen Chen <caxche@gmail.com>
2024-07-07 15:17:57 +0800

    add logo

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -33,8 +33,8 @@ export default function Footer() {
 
   const SupportLinks = [
     {
-      title: t('tap4'),
-      href: 'https://www.tap4.ai',
+      title: t('Afmobi'),
+      href: 'https://afmobi.com',
     },
     {
       title: t('tattoo'),
